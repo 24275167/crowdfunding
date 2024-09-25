7cd54cc4a7352ebc92b1243f11b12a18973db159
24275167 <145706180+24275167@users.noreply.github.com>
2024-09-25 14:20:20 +0800

    添加到api

--- crowdfunding_db.js
+++
@@ -1,9 +0,0 @@
-import { createConnection } from 'mysql2';
-// mysql连接
-const mysql = createConnection({
-  host: 'localhost',       
-  user: 'root',
-  password: '123456',
-  database: 'crowdfunding_db',
-}).promise();
-export default mysql
