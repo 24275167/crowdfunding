0ea04a639e0008fc37de4e0087aef33c2c2cc120
24275167 <145706180+24275167@users.noreply.github.com>
2024-09-25 16:42:28 +0800

    代码修复

--- api/main.js
+++ api/main.js
@@ -9,18 +9,42 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // 配置页面
 app.use(express.static(path.join(__dirname, '../client')));
+
+// 数据处理
+async function dataHandler(FUNDRAISER) {
+    if (FUNDRAISER.length) {
+        const [CATEGORY] = await mysql.query('SELECT * FROM CATEGORY');
+        return FUNDRAISER.map(item => {
+            const category = CATEGORY.find(c => c.CATEGORY_ID === item.CATEGORY_ID);
+            item['CATEGORY_NAME'] = category ? category.NAME : null;  // 提取并赋值类别名称
+            return item;
+        });
+    }
+    return FUNDRAISER;
+}
+
+
 // 列表
 app.get("/fundraisers", async (req, res) => {
-    mysql.query('SELECT * FROM FUNDRAISER').then(data=>{
-        res.status(200).send(data[0])
-    }).catch(()=>res.status(500))
+    try {
+        const [data] = await mysql.query('SELECT * FROM FUNDRAISER');
+        const processedData = await dataHandler(data);
+        res.status(200).send(processedData);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
+
 // 分类
 app.get("/categories", async (req, res) => {
-    mysql.query("SELECT * FROM CATEGORY").then(data=>{
-        res.status(200).send(data[0])
-    }).catch(()=>res.status(500))
+    try {
+        const [data] = await mysql.query("SELECT * FROM CATEGORY");
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
+
 // 搜索
 app.get("/search", async (req, res) => {
     const { organizer, city, category } = req.query;
@@ -34,16 +58,26 @@ app.get("/search", async (req, res) => {
     if (category) {
         sql += sql ? 'AND' : 'WHERE' + ` CATEGORY_ID = ${category}`
     }
-    mysql.query(`SELECT * FROM FUNDRAISER ` + sql).then(data=>{
-        res.status(200).send(data[0])
-    }).catch(()=>res.status(500))
+    try {
+        const [data] = await mysql.query(`SELECT * FROM FUNDRAISER` + sql);
+        const processedData = await dataHandler(data);
+        res.status(200).send(processedData);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
+
 // 获取详情
 app.get("/fundraiser/:id", async (req, res) => {
-    mysql.query(`SELECT * FROM FUNDRAISER WHERE FUNDRAISER_ID = ${req.params.id}`).then(data=>{
-        res.status(200).send(data[0])
-    }).catch(()=>res.status(500))
+    try {
+        const [data] = await mysql.query(`SELECT * FROM FUNDRAISER WHERE FUNDRAISER_ID = ?`, [req.params.id]);
+        const processedData = await dataHandler(data);
+        res.status(200).send(processedData);
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
 });
+
 // 启动
 app.listen(3000, () => {
     console.log(`http://localhost:3000/index.html`);
